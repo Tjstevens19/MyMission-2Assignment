b78fcbba41cbc420e373beaa7af0e36550c84980
Tjstevens19 <tdogstrike@gmail.com>
2024-01-17 19:12:45 -0700

    create a DiceRoller.cs and move the class of the same name into that file.

---
+++ DiceRoller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerNamespace
+{
+    // Create a diceRoller class
+    internal class DiceRoller
+    {
+        // Create a random number generator
+        private readonly Random random;
+
+        public DiceRoller()
+        {
+            random = new Random();
+        }
+
+        // Create a method to roll a dice
+        public int[] RollDice(int numberOfRolls)
+        {
+            // Create an array to keep track of the number of times each combination is thrown
+            int[] diceRolls = new int[13];
+
+            // Roll the dice the specified number of times
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                // Roll the dice
+                int dice1 = random.Next(1, 7); // 1-6
+                int dice2 = random.Next(1, 7); // 1-6
+
+                // Calculate the sum of the dice
+                int sum = dice1 + dice2;
+
+                // Increment the number of times the combination was thrown
+                diceRolls[sum]++;
+            }
+
+            // Return the dice roll
+            return diceRolls;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿internal class Program
+﻿using DiceRollerNamespace;
+public class Program
 {
     private static void Main(string[] args)
     {
@@ -35,39 +36,5 @@
         Console.WriteLine($"Total number of rolls: {numberOfDiceRolls}.");
     }
     
-    // Create a diceRoller class
-    public class DiceRoller
-    {
-        // Create a random number generator
-        private readonly Random random;
-
-        public DiceRoller()
-        {
-            random = new Random();
-        }
-
-        // Create a method to roll a dice
-        public int[] RollDice(int numberOfRolls)
-        {
-            // Create an array to keep track of the number of times each combination is thrown
-            int[] diceRolls = new int[13];
-
-            // Roll the dice the specified number of times
-            for (int i = 0; i < numberOfRolls; i++)
-            {
-                // Roll the dice
-                int dice1 = random.Next(1, 7); // 1-6
-                int dice2 = random.Next(1, 7); // 1-6
-
-                // Calculate the sum of the dice
-                int sum = dice1 + dice2;
-
-                // Increment the number of times the combination was thrown
-                diceRolls[sum]++;
-            }
-
-            // Return the dice roll
-            return diceRolls;
-        }
-    }
+    
 }
